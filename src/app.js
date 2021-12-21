5da0c8fd28a7b9acef715abea12e75bb3cbb6c5a
lu0ngb3ng7f <lu0ngb3ng7f@hotmail.com>
2021-12-21 11:17:07 +0100

    Added config files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
+const exec = require('shelljs.exec');
 const prompts = require('prompts');
 const chalk = require('chalk');
 
@@ -17,8 +18,7 @@ const chalk = require('chalk');
       { title: 'Add .gitattributes', value: 'gitattributes' },
       { title: 'Add .gitignore', value: 'gitignore', selected: true },
       { title: 'Add README', value: 'readme', selected: true },
-      { title: 'Initialize Git', value: 'initgit', selected: true },
-      { title: 'Initialize default NPM', value: 'initnpm', selected: true }
+      { title: 'Initialize Git', value: 'initgit', selected: true }
     ],
     initial: null,
     instructions: false,
@@ -26,53 +26,6 @@ const chalk = require('chalk');
   
   var response = await prompts(question);
   response = response.includes;
-
-  response.forEach(element => {
-    
-    switch(element) {
-
-      case 'tailwindcss':
-        break;
-
-      case 'snowpack':
-        break;
-
-      case 'nodemon':
-        break;
-
-      case 'gitattributes':
-        var gitattributes = shell.cat(__dirname + '/config/.gitignore');
-        gitattributes = gitattributes.stdout;
-
-        break;
-
-      case 'gitignore':
-        var gitignore = shell.cat(__dirname + '/config/.gitignore');
-        gitignore = gitignore.stdout;
-
-        break;
-
-      case 'readme':
-        break;
-
-      case 'initgit':
-        console.log('\r\n' + chalk.greenBright('√') + chalk.white.bold(' Message from Git:'));
-
-        if(shell.exec('git init').code !== 0) {
-          console.log(chalk.red.bold('Error initialising Git repo'));
-        }
-        break;
-
-      case 'initnpm':
-        console.log('\r\n' + chalk.greenBright('√') + chalk.white.bold(' Message from NPM:'));
-
-        if(shell.exec('npm init -y').code !== 0){
-          console.log(chalk.red.bold('Error initialising NPM package'));
-        }
-        break;
-
-    }
-  });
 
   shell.mkdir('-p', [
     './views',
@@ -82,5 +35,93 @@ const chalk = require('chalk');
     './controllers',
     './routers'
   ]);
+
+  fs.writeFileSync('./views/.gitignore', '');
+  fs.writeFileSync('./assets/lib/.gitignore', '');
+  fs.writeFileSync('./assets/css/.gitignore', '');
+  fs.writeFileSync('./assets/js/.gitignore', '');
+  fs.writeFileSync('./assets/img/.gitignore', '');
+  fs.writeFileSync('./models/.gitignore', '');
+  fs.writeFileSync('./controllers/.gitignore', '');
+  fs.writeFileSync('./routers/.gitignore', '');
+  
+  if(exec('npm init -y').code !== 0){
+    console.log(chalk.red.bold('Error initialising NPM package'));
+  }
+  
+  response.forEach(element => {
+    
+    switch(element) {
+
+      case 'tailwindcss':
+        shell.mkdir('-p', ['./src']);
+        exec('npm install --save-dev tailwindcss');
+
+        const tailwindcss = shell.cat(__dirname + '/config/tailwind.css').stdout;
+        const tailwindConfig = shell.cat(__dirname + '/config/tailwind.config.js').stdout;
+
+        fs.writeFile('./src/tailwind.css', tailwindcss, (err) => {
+          if(err) console.log(chalk.red.bold(`Error writing tailwind.css file`));
+        });
+
+        fs.writeFile('./tailwind.config.js', tailwindConfig, (err) => {
+          if(err) console.log(chalk.red.bold(`Error writing tailwind.config.js file`));
+        })
+
+        break;
+
+      case 'snowpack':
+        exec('npm install --save-dev snowpack');
+
+        const snowpackConfig = shell.cat(__dirname + '/config/snowpack.config.js');
+
+        fs.writeFile('./snowpack.config.js', snowpackConfig, (err) => {
+          if(err) console.log(chalk.red.bold('Error writing snowpack.config.js file'));
+        })
+        break;
+
+      case 'nodemon':
+        exec('npm install --save-dev nodemon');
+
+        const nodemonConfig = shell.cat(__dirname + '/config/nodemon.json').stdout;
+
+        fs.writeFile('./nodemon.json', nodemonConfig, (err) => {
+          if(err) console.log(chalk.red.bold(`Error writing nodemon.json file`));
+        });
+        
+        break;
+
+      case 'gitattributes':
+        const gitattributes = shell.cat(__dirname + '/config/.gitattributes').stdout;
+
+        fs.writeFile('./.gitattributes', gitattributes, (err) => {
+          if(err) console.log(chalk.red.bold(`Error writing .gitattributes file`));
+        });
+
+        break;
+
+      case 'gitignore':
+        const gitignore = shell.cat(__dirname + '/config/.gitignore').stdout;
+
+        fs.writeFile('./.gitignore', gitignore, (err) => {
+          if(err) console.log(chalk.red.bold(`Error writing .gitignore file`));
+        })
+
+        break;
+
+      case 'readme':
+        fs.writeFile('./README.md', '', (err) => {
+          if(err) console.log(chalk.red.bold('Error writing README.md file'));
+        })
+        break;
+
+      case 'initgit':
+        if(exec('git init').code !== 0) {
+          console.log(chalk.red.bold('Error initialising Git repo'));
+        }
+        break;
+
+    }
+  });
 
 })();
---
+++ src/config/snowpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mount: {
+    views: '/',
+    assets: { url: '/assets',  static: true },
+  },
+  devOptions: {
+    port: 3000,
+  },
+  exclude: [],
+  buildOptions: {
+    out: './',
+    metaUrlPath: './assets/lib',
+  },
+};
